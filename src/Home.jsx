31e92a319f4ed34ff33332b45bed93880d660129
ankitsoniblogger <ankitsoniblogger@gmail.com>
2022-04-18 00:36:43 +0530

    main

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return(
+  <>
+  <h1 className='title'>Home page</h1>
+   
+  
+  </>
+  )
+}
+
+const Hello =() => {
+    return <h1>Hello</h1>;
+}
+
+export {Home, Hello}
