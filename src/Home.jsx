0aee2d19f562537568d43bd380d47bbfa6796dec
ankitsoniblogger <ankitsoniblogger@gmail.com>
2022-04-18 23:48:41 +0530

    btn

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,14 +5,15 @@ const Home = () => {
   return(
   <>
   <h1 className='title'>Home page</h1>
-   
+  
+   <button className='btn btn-primary' > Hello</button>
   
   </>
   )
 }
 
 const Hello =() => {
-    return <h1>Hello</h1>;
+    return <h1 className='title'>Hello</h1>;
 }
 
 export {Home, Hello}
